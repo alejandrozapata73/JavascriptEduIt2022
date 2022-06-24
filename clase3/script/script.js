91ea5e1acea59b296936a2302598fb246a161dac
alejandro <alejandrozapata73@gmail.com>
2022-06-23 21:07:49 -0300

    archivos clase 3

---
+++ clase3/script/script.js
@@ -0,0 +1,266 @@
+
+
+// //obtener datos
+// var nombre = "juan Perez"
+// var nota1= 8
+// var nota2= 8
+// var nota3= 6
+// //operacion 1
+
+// var promedio =  (nota1+nota2-nota3)/4
+
+
+// console.log("el nombre es: " +nombre+" y su promedio es: "+promedio)
+
+// //alert("el promedio es: " + promedio)
+
+// console.log("Federico")
+
+// var alumnosMartes = "juan, marcos, amparo"
+
+// console.log(alumnosMartes)
+
+// alumnosMartes = "ignacio, javier, emanuel"
+
+// console.log(alumnosMartes)
+
+// alumnosMartes = "Walter, Rodrigo, Nestor"
+
+// console.log(alumnosMartes)
+
+// alumnosMartes = 1001
+
+// console.log(alumnosMartes)
+
+
+// const PI = "3.141516"
+
+// var edad = 34
+
+// const DOLAR = true
+
+// console.log(PI)
+
+// var numeroA = 10
+// //modulo...%
+
+// console.log(10/2)//5
+// console.log(10%3)// 10/3= 3...1
+// console.log(4%2)//0..par
+// console.log(5%2)//1..impar
+// console.log(6%2)//0
+// console.log(10%7)//
+
+
+// // const EDADF = prompt("ingrese su edad")
+
+// // console.log("El usuario ingreso su nombre: "+ usuario)
+// // alert("El usuario ingreso: " + usuario)
+// // EDADF = 15
+// // alert(" y su edad es" + EDADF)
+
+// // cuando pido por prompt.. lo guarda como dato string
+// var numeroA = prompt("ingrese un valor a sumar")
+
+// console.log(numeroA + numeroA)//concatena
+
+// numeroA = Number(numeroA)
+
+// console.log(numeroA + numeroA)//suma
+// //convertir ese valor (string)
+
+
+//console.log(numeroA + numeroA)
+
+//algoritmo.... para sacar el perimetro de un cuadrado => todos los lados iguales
+
+// var lado = Number(prompt("ingrese un lado del cuadrado"))
+
+// console.log(lado)
+
+// const PERIMETRO = lado * 4
+
+
+// console.log(PERIMETRO)
+
+// console.log("el perimetro del cuadro es: " + PERIMETRO)
+
+// var producto = prompt("Ingrese el pedido")
+// var cantidad = parseInt(prompt("ingrese la cantidad"))
+// //console.log(cantidad)
+// const PRECIO = 120
+
+// const TOTAL = PRECIO * cantidad
+
+// //como sacar el 10% del total
+// const PROPINA = 0.1*TOTAL // 
+
+// const TOTALPROPINA = PROPINA + TOTAL
+
+// console.log("el usuario pidio una: " + producto)
+// console.log("Cantidad: " + cantidad)
+// console.log("precio unitario: " + PRECIO)
+// console.log("Total a pagar: " + TOTAL)
+// console.log("la propina es: " + PROPINA)
+// console.log("el total total :" + TOTALPROPINA)
+
+
+var nombre = "ignacio"
+var edad = 25
+const DNI = 31698547
+const FECH_NAC = "23/07/1984"
+nombre = "miguel"
+
+//operadores de relacion/relacionales
+/*
+mayor >
+mayor igual >=
+menor <
+menor igual <=
+igual ==
+igual estricto ===
+distinto !=
+*/
+//si es verdad 5 es mayor a 3
+// console.log(5>3)// del tipo booleano true
+// console.log(5<10)// true
+// console.log(" 5 > 6: " + (5>6))// false
+// console.log(" 5 > 6: " + (5>6))// false
+// console.log(5>=5)//
+// console.log(5==5)//
+// console.log(5==4)
+// console.log(5!=6)//false
+// console.log(1=="1")
+// console.log(1==="1")
+// console.log(1=="1 pizza")
+// console.log(1===true)
+// console.log(false==true)
+// console.log("123456"=="123456")//true
+// console.log("a"=="a")//
+// console.log("A"=="a")//
+// console.log("manzana"!="Manzana")//
+
+// var usuario = prompt("ingrese su usuario")
+// var password = Number(prompt("ingrese su password de tipo numero"))
+
+
+// const PASSREAL = "siempreviva"
+// const USSER = "brian"
+
+
+// var verificacion = password===PASSREAL
+// var verificacionUsser = usuario==USSER
+
+// console.log("pass result: " + verificacion)
+// console.log("usuario result: "+ verificacionUsser)
+
+//condicionales..
+/*
+
+si no llego a las 7...llego tarde, 
+arranco temprano
+
+si no tomo el colectivo en horario...llego tarde
+llego temprano
+
+si no suena la alarma...duermo mas
+
+
+*/
+
+//... si es true
+//false
+
+
+// var nombreUsser = prompt("ingrese su nombre")
+// var edadUsser = parseInt(prompt("ingrese su edad"))
+
+// const PERMISO = 18
+
+// if(edadUsser>PERMISO){
+//     alert("Puede entrar al Bar: " + nombreUsser)
+//     alert("y su edad es: " + edadUsser)
+//     console.log("Puede entrar...")
+
+//     if(nombreUsser=="juan"){
+//         alert("si sos juan sos cliente vip 2x1")
+//     }else{
+//         alert("no sos un usuario vip")
+//     }
+    
+// }else{
+//     alert("No puede entrar")
+//     console.log("no puede entrar")
+// }
+
+
+var nombreUsser = prompt("ingrese su usuario")
+var passUsser = prompt("password")
+
+const USSER = "rodrigo"
+const PASS = "pepe01"
+
+//nombre usuairo
+// if(nombreUsser==USSER){
+
+//     alert("bienvenido: " + nombreUsser)
+//     document.write("Bienvenido: " + nombreUsser)
+
+// }else{
+  
+//     alert("usuario incorrecto")
+//     console.log("usuario incorrecto")
+//     document.write("usuario incorrecto")
+// }
+
+// if(passUsser==PASS){
+
+//     alert("PASS CORRECTO: " )
+//     document.write("PASS C")
+
+// }else{
+  
+//     alert("PASS INCORRECTO")
+   
+// }
+
+
+/*
+
+le pide al usuario una letra
+
+letra == letra
+
+var c = "c"
+
+var a = "a"
+
+var s = "s"
+
+var a = "a"
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
