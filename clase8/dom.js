f2dbf2b89c8eb414096b0fc610071d7e1c56441d
alejandro <alejandrozapata73@gmail.com>
2022-07-12 21:02:47 -0300

    subiendo ultimos cambios

--- clase8/dom.js
+++ clase8/dom.js
@@ -2,70 +2,126 @@
 
 
 
-//hoisting
-//funcione declarativa
-function sumarNumeros(numeroA=10,numeroB=40,boleano=true){
+// //hoisting
+// //funcione declarativa
+// function sumarNumeros(numeroA=10,numeroB=40,boleano=true){
 
-    var suma = numeroA + numeroB
+//     var suma = numeroA + numeroB
 
-    console.log(boleano)
-    console.log(suma)
+//     console.log(boleano)
+//     console.log(suma)
+
+// }
+
+// sumarNumeros()
+
+// console.log(sumarNumeros())
+
+// function sumarNumerosA(numeroA,numeroB){
+
+//     return numeroA + numeroB
+// }
+
+// console.log(sumarNumerosA(10,20))
+
+
+// var almacenarRetorno = sumarNumerosA(2,10)
+
+// console.log(almacenarRetorno)
+
+// if(almacenarRetorno>15){
+//     alert("esta funcion retorna mayor a 20")
+// }else{
+//     alert("esta funcion retorna menos que 20")
+// }
+
+
+// function mayor(edad){
+
+//     if(edad>18){
+
+//          "el resultado es: "+ true
+
+       
+
+//     }else{
+
+       
+//     }
+
+
+//     return 15
+
+    
+// }
+
+// var juan = mayor(20)
+
+// var miguel = mayor(12)
+
+// var carlos = mayor(25)
+
+// console.log(juan)
+// console.log(miguel)
+// console.log(carlos)
+
+// //objeto... atributos y caracteriscas
+
+
+
+function saludando(nombre){
+
+    alert("hola soy un alert interno" + nombre)
 
 }
 
-sumarNumeros()
+function contarPalabras(){
 
-console.log(sumarNumeros())
 
-function sumarNumerosA(numeroA,numeroB){
-
-    return numeroA + numeroB
 }
 
-console.log(sumarNumerosA(10,20))
 
 
-var almacenarRetorno = sumarNumerosA(2,10)
+//document objet model
+//JavaScript Vanilla 
+//
 
-console.log(almacenarRetorno)
+// console.log(document.doctype)
+// console.log(document.body)
+// console.log(document.head)
+// console.log(document.all)//
+// console.log(document.all[12])
 
-if(almacenarRetorno>15){
-    alert("esta funcion retorna mayor a 20")
+console.log(document.getElementById('original1'))
+console.log(document.getElementById('original2'))
+
+document.getElementById('original1').textContent = "Rodrigo"
+
+
+var edad=12
+
+if(edad>18){
+
+document.getElementById('header-title').textContent = "Emmanuel"
+
 }else{
-    alert("esta funcion retorna menos que 20")
-}
-
-
-function mayor(edad){
-
-    if(edad>18){
-
-         "el resultado es: "+ true
-
-       
-
-    }else{
-
-       
-    }
-
-
-    return 15
-
+ 
+    document.getElementById('header-title').textContent = "No puede visitar"
     
 }
 
-var juan = mayor(20)
 
-var miguel = mayor(12)
+// document.getElementById('input3').value = "Cafecito poderoso"
 
-var carlos = mayor(25)
+function asignarValor(){
 
-console.log(juan)
-console.log(miguel)
-console.log(carlos)
+    var valorInput = document.getElementById('input3').value
 
-//objeto... atributos y caracteriscas
+    document.getElementById('divSolo').textContent = valorInput
+
+}
+
+
 
 
 
